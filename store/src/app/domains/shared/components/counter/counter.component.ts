6714e796fe5c32aed1ac50dfa151fc98eb8ba16f
AngelSanchezT <ajsanchez22@gmail.com>
2023-11-25 20:05:49 -0500

    Ciclo de vida de componentes

---
+++ store/src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required: true}) duration : number = 0;
+  @Input({required: true}) message : string = '';
+
+  constructor() {
+    // NO ASYNC
+    // before render
+    console.log('constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // before and during render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
+}
