96b02bd3a49b889b75292ff1de1291504d270855
AngelSanchezT <ajsanchez22@gmail.com>
2023-11-28 23:24:39 -0500

    Update About - Add Audio

--- store/src/app/domains/info/pages/about/about.component.ts
+++ store/src/app/domains/info/pages/about/about.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CounterComponent } from '../../../shared/components/counter/counter.component';
+import { WaveInfoComponent } from '../../components/wave-info/wave-info.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, CounterComponent],
+  imports: [CommonModule, CounterComponent, WaveInfoComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
@@ -16,12 +17,12 @@ export class AboutComponent {
 
   changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
-    this.duration.set(input.valueAsNumber) 
+    this.duration.set(input.valueAsNumber)
   }
 
   changeMessage(event: Event) {
     const input = event.target as HTMLInputElement;
-    this.message.set(input.value) 
+    this.message.set(input.value)
   }
 
 }
