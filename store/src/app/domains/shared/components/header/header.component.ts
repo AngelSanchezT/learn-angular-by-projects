e7136cf770bbec6caba335ceeebac839418dd14b
AngelSanchezT <ajsanchez22@gmail.com>
2023-11-30 02:37:19 -0500

    Calculate total the cart with ngOnChanges

--- store/src/app/domains/shared/components/header/header.component.ts
+++ store/src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Product } from '../../models/product.model';
@@ -13,9 +13,21 @@ import { Product } from '../../models/product.model';
 export class HeaderComponent {
   hideSideMenu = signal(true);
   @Input({required: true}) cart: Product[] = [];
+  total = signal(0);
 
   toogleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const cart = changes['cart'];
+    if(cart){
+      this.total.set(this.calcTotal())
+    }
+  }
+
+  calcTotal() {
+    return this.cart.reduce((total, product) => total + product.price, 0 );
   }
 
 }
