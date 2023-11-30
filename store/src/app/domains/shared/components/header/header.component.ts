1bd69fda1de72a05b472fc3b233726933d0de268
AngelSanchezT <ajsanchez22@gmail.com>
2023-11-30 02:22:12 -0500

    Update Cart

--- store/src/app/domains/shared/components/header/header.component.ts
+++ store/src/app/domains/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +12,7 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   hideSideMenu = signal(true);
+  @Input({required: true}) cart: Product[] = [];
 
   toogleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState);
